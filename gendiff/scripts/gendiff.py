e4fbd69b647510741ea00bb0694ccc01fee51ef2
Dmitriy Bulygin <demetrius.storm@gmail.com>
2020-06-03 18:32:22 +0300

    remove wemake-python-styleguid, add flake8

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,3 +1,5 @@
+"""First file used argparse and it module."""
+
 import argparse
 
 parser = argparse.ArgumentParser(description='Generate diff')
@@ -6,9 +8,9 @@ parser.add_argument('second_file')
 args = parser.parse_args()
 
 
-
-def main():
-    print('Welcome to the Gendiff!')
+def main(name='Dima'):
+    """Welcome function."""
+    print(f'Welcome to Gendiff, {name}!')
 
 
 if __name__ == '__main__':
