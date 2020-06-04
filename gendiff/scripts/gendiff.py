343dec479f3beef4618fd1ef5081952e929fe240
Demetrius Storm <47479409+DemetriusStorm@users.noreply.github.com>
2020-06-04 12:24:35 +0300

    Update gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -5,17 +5,25 @@ and outputs the result in the specified format.
 
 import argparse
 
-parser = argparse.ArgumentParser(description='Generate diff')
-parser.add_argument('first_file')
-parser.add_argument('second_file')
-parser.add_argument('-f', '--format', help='set format of output')
-args = parser.parse_args()
 
+def generate_diff(first_file='', second_file='', format_file='json'):
+    """
+    receive two position arguments (/path/to/file1, /path/to/file2)
+    and out file format (-f [json []])
 
-def main(name='Dima'):
-    """Welcome function."""
-    print(f'Welcome to Gendiff, {name}!')
+    :param first_file:
+    :param second_file:
+    :param format_file:
+    :return: string result difference files
+    """
+    parser = argparse.ArgumentParser(description='Generate diff')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.add_argument('-f', '--format', help='set format of output')
+
+    return parser.parse_args()
 
 
 if __name__ == '__main__':
-    main()
+    generate_diff()
+
