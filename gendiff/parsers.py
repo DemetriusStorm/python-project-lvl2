206c15fe7df9bab9aba45951985d6eb008c2d310
Dmitriy Bulygin <demerius.storm@gmail.com>
2020-10-14 13:59:52 +0300

    WPS modify

--- gendiff/parsers.py
+++ gendiff/parsers.py
@@ -1,35 +1,29 @@
 """Parsers."""
 
-import os
 import json
+import os
+
 import yaml
 
-EXTENSIONS = (
-    JSON,
-    YAML,
-    YML,
-) = (
-    '.json',
-    '.yaml',
-    '.yml',
-)
+EXTENSIONS = ('.json', '.yaml', '.yml')
 
 
 def result_parser(file_ext, source):
     """
-    Result dump data from source.
+    Get result dump data from source.
 
     Parameters:
         file_ext: file extension
         source: source
 
-    Returns: dump
+    Returns:
+        return object
     """
-    with open(source, 'r') as f:
-        if file_ext == JSON:
-            return json.load(f)
-        elif file_ext in (YAML, YML):
-            return yaml.safe_load(f)
+    with open(source, 'r') as source_data:
+        if file_ext == '.json':
+            return json.load(source_data)
+        elif file_ext in EXTENSIONS:
+            return yaml.safe_load(source_data)
 
 
 def get_data(source_file):
@@ -37,14 +31,15 @@ def get_data(source_file):
     Load data from file.
 
     Parameters:
-        source_file:
+        source_file: source_file
 
-    Returns: data
+    Returns:
+        return data from result parser
+
+    Raises:
+        Exception: extension not in supported ext
     """
-    file_ext = os.path.splitext(source_file)[1]
-    if file_ext not in EXTENSIONS:
-        raise Exception('Format {file_ext} is not supported.'.format(
-            file_ext=file_ext,
-        ))
-    else:
-        return result_parser(file_ext, source_file)
+    ext = os.path.splitext(source_file)[1]
+    if ext in EXTENSIONS:
+        return result_parser(ext, source_file)
+    raise Exception('Format {0} is not supported.'.format(ext))
