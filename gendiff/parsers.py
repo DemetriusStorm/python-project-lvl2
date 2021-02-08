968ff71884c5dfcd9e7b672982da4c28e2751e94
Dmitriy Bulygin <demerius.storm@gmail.com>
2021-02-08 10:20:20 +0300

    parsers.py refactoring - put the work with external sources into a separate module

--- gendiff/parsers.py
+++ gendiff/parsers.py
@@ -1,29 +1,8 @@
 """Parsers."""
 
-import json
 import os
 
-import yaml
-
-EXTENSIONS = ('.json', '.yaml', '.yml')
-
-
-def result_parser(file_ext, source):
-    """
-    Get result dump data from source.
-
-    Parameters:
-        file_ext: file extension
-        source: source
-
-    Returns:
-        return object
-    """
-    with open(source, 'r') as source_data:
-        if file_ext == '.json':
-            return json.load(source_data)
-        elif file_ext in EXTENSIONS:
-            return yaml.safe_load(source_data)
+from gendiff import reader_ext_source as reader
 
 
 def get_data(source_file):
@@ -40,6 +19,6 @@ def get_data(source_file):
         Exception: extension not in supported ext
     """
     ext = os.path.splitext(source_file)[1]
-    if ext in EXTENSIONS:
-        return result_parser(ext, source_file)
+    if ext in reader.EXTENSIONS:
+        return reader.result_parser(ext, source_file)
     raise Exception('Format {0} is not supported.'.format(ext))
