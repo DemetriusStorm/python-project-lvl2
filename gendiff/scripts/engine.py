1bb8c249362fbb95021a3489833d2f3c3d027fa1
Dmitriy Bulygin <demerius.storm@gmail.com>
2021-02-09 09:40:13 +0300

    Took the logic out of the main function.

--- gendiff/scripts/engine.py
+++ gendiff/scripts/engine.py
@@ -2,19 +2,20 @@
 
 from gendiff import parsers, formats, gendiff, reader_ext_source
 
+args = reader_ext_source.parser.parse_args()
+
+if args.format == formats.JSON:
+    format_diff = formats.render_json
+elif args.format == formats.PLAIN:
+    format_diff = formats.render_plain
+elif args.format is None:
+    format_diff = formats.render_simple
+else:
+    raise Exception('Format is not supported.')
+
 
 def main():
     """Generate result diff function."""
-    args = reader_ext_source.parser.parse_args()
-    if args.format == formats.JSON:
-        format_diff = formats.render_json
-    elif args.format == formats.PLAIN:
-        format_diff = formats.render_plain
-    elif args.format is None:
-        format_diff = formats.render_simple
-    else:
-        raise Exception('Format is not supported.')
-
     first_file = parsers.get_data(args.first_file)
     second_file = parsers.get_data(args.second_file)
     print(format_diff(gendiff.generate_diff(first_file, second_file)))
