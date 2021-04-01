0f78cf22edc12f07d8a6d80916e808898c149b5c
Dmitriy Bulygin <demerius.storm@gmail.com>
2021-04-01 12:44:37 +0300

    test refactor

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -6,9 +6,9 @@ import pytest
 
 from gendiff.gendiff import generate_diff
 
-json_file = 'tests/fixtures/json_result'
-plain_file = 'tests/fixtures/plain_result'
-stylish_file = 'tests/fixtures/stylish_result'
+json_result = 'tests/fixtures/json_result'
+plain_result = 'tests/fixtures/plain_result'
+stylish_result = 'tests/fixtures/stylish_result'
 
 
 def _read_data(file_name):
@@ -22,11 +22,11 @@ def _read_data(file_name):
 def test_gen_diff(file_format):
     file1 = 'tests/fixtures/first_file.{0}'.format(file_format)
     file2 = 'tests/fixtures/second_file.{0}'.format(file_format)
-    source_data = generate_diff(file1, file2, 'json')
+    json_diff = generate_diff(file1, file2, 'json')
+    stylish_diff = generate_diff(file1, file2, 'stylish')
+    plain_diff = generate_diff(file1, file2, 'plain')
 
-    assert generate_diff(file1, file2) == _read_data(stylish_file)
-    assert generate_diff(file1, file2, 'stylish') == _read_data(
-        stylish_file,
-    )
-    assert generate_diff(file1, file2, 'plain') == _read_data(plain_file)
-    assert isinstance(json.loads(source_data), dict)
+    assert stylish_diff == _read_data(stylish_result)
+    assert plain_diff == _read_data(plain_result)
+    assert json.loads(json_diff) == json.loads(_read_data(json_result))
+    assert isinstance(json.loads(json_diff), dict)
